fd3a0d81536ba38ed9d497112de9d6475e8f4936
Joao Amaral <joaovillanova@hotmail.com.br>
2020-12-14 22:37:38 -0300

    Salvando novos usuarios no banco

--- src/main/java/org/epineuro/EpineuroApplication.java
+++ src/main/java/org/epineuro/EpineuroApplication.java
@@ -1,5 +1,6 @@
 package org.epineuro;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -37,7 +38,7 @@ public class EpineuroApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		if(permissionRepository.findAll().size() == 0) {
+		if(permissionRepository.findAll().size() < 9) {
 			
 			Permission p1 = new Permission();
 			
@@ -61,6 +62,7 @@ public class EpineuroApplication implements CommandLineRunner {
 			groupRepository.save(g1);
 			
 			User u1 = User.builder()
+					.id(1L)
 					.name("João Amaral")
 					.email("joaovillanova@hotmail.com.br")
 					.password(passwordEncoder.encode("123456"))
@@ -69,7 +71,87 @@ public class EpineuroApplication implements CommandLineRunner {
 					.cod(1L)
 			.build();
 			
-			userRepository.save(u1);
+			User u2 = User.builder()
+					.id(2L)
+					.name("Fernanda Dala Costa")
+					.email("nandacdcosta@gmail.com")
+					.password(passwordEncoder.encode("123456"))
+					.group(groupRepository.findById(1L).get())
+					.type(1L)
+					.cod(1L)
+			.build();
+			
+			User u3 = User.builder()
+					.id(3L)
+					.name("Fernando Bellinaso")
+					.email("fernandobellinaso@gmail.com")
+					.password(passwordEncoder.encode("123456"))
+					.group(groupRepository.findById(1L).get())
+					.type(1L)
+					.cod(1L)
+			.build();
+			
+			User u4 = User.builder()
+					.id(4L)
+					.name("Luís Guilherme ADCR")
+					.email("lgadcr@hotmail.com")
+					.password(passwordEncoder.encode("123456"))
+					.group(groupRepository.findById(1L).get())
+					.type(1L)
+					.cod(1L)
+			.build();
+			
+			User u5 = User.builder()
+					.id(5L)
+					.name("Juliana Silveira")
+					.email("julianaofs@yahoo.com.br")
+					.password(passwordEncoder.encode("123456"))
+					.group(groupRepository.findById(1L).get())
+					.type(1L)
+					.cod(1L)
+			.build();
+			
+			User u6 = User.builder()
+					.id(6L)
+					.name("Michele Fighera")
+					.email("mrfighera23@gmail.com")
+					.password(passwordEncoder.encode("123456"))
+					.group(groupRepository.findById(1L).get())
+					.type(1L)
+					.cod(1L)
+			.build();
+			
+			User u7 = User.builder()
+					.id(7L)
+					.name("Mônika dos Santos")
+					.email("monikadossantos@gmail.com")
+					.password(passwordEncoder.encode("123456"))
+					.group(groupRepository.findById(1L).get())
+					.type(1L)
+					.cod(1L)
+			.build();
+						
+			User u8 = User.builder()
+					.id(8L)
+					.name("Rafaela Rossini")
+					.email("rafarossini@hotmail.com")
+					.password(passwordEncoder.encode("123456"))
+					.group(groupRepository.findById(1L).get())
+					.type(1L)
+					.cod(1L)
+			.build();
+			
+			User u9 = User.builder()
+					.id(8L)
+					.name("Larissa Blás")
+					.email("lariblas11@gmail.com")
+					.password(passwordEncoder.encode("123456"))
+					.group(groupRepository.findById(1L).get())
+					.type(1L)
+					.cod(1L)
+			.build();
+			
+			userRepository.saveAll(Arrays.asList(u1,u2,u3,u4,u5,u6,u7,u8,u9));
 			
 		}
 		
