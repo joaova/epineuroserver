dfb1f4a3268aab2d233dfee807abbde1da888362
Joao Amaral <joaovillanova@hotmail.com.br>
2021-08-29 21:24:43 -0300

    Enum diseasegroup

--- src/main/java/org/epineuro/enums/DiseaseGroup.java
+++ src/main/java/org/epineuro/enums/DiseaseGroup.java
@@ -7,7 +7,15 @@ public enum DiseaseGroup {
 	EPILEPSY(2, "Epilepsia"),
 	DEMENTIA(3, "Demências"),
 	CEREBROVASCULAR_DISEASE(4, "Doenças Cerebrovasculares"),
-	NEUROMUSCULAR_DISEASE(5, "Doenças neuromusculares");
+	NEUROMUSCULAR_DISEASE(5, "Doenças neuromusculares"),
+	NEUROINFECTIOUS_DISEASE(6, ""),
+	SPINAL_CORD(7, "Doenças da medula espinal"),
+	PNS_AND_MOTOR_NEURON(8, "Doenças do sistema nervoso periférico e do neurônio motor"),
+	SLEEP_DISORDERS(9, "Doenças do sono"),
+	AUTONOMIC_DISEASE(10, "Distúrbios autonômicos"),
+	NEURO_OTOLOGY(11, "Neuro-otologia"),
+	MS_AND_INFLAMATORY(12, "Esclerose múltipla e outras doenças inflamatórias"),
+	NEURO_ONCOLOGY(13, "Neuro-oncologia");
 	
 	private int cod;
 	private String description;
